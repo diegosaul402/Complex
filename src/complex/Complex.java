b4bc00286efe1ccb7bcd2c49ec15154a61839187
Diego Mendoza <diego_saul_402@hotmail.com>
2016-08-29 17:54:44 -0500

    first functional code

---
+++ src/complex/Complex.java
@@ -0,0 +1,32 @@
+package complex;
+
+public class Complex {
+	private double real;
+	private double img;
+	public Complex() {
+		
+	}
+	public Complex(double a, double b) {
+		// TODO Auto-generated method stub
+		this.real = a;
+		this.img = b;
+
+	}
+
+	public double getReal() {
+		return real;
+	}
+
+	public void setReal(double real) {
+		this.real = real;
+	}
+
+	public double getImg() {
+		return img;
+	}
+
+	public void setImg(double img) {
+		this.img = img;
+	}
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+import complex.Complex;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Complex complejo = new Complex(3,1);
+		System.out.println("R: " + complejo.getReal() + ", I: " + complejo.getImg());
+
+	}
+
+}
